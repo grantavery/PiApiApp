c4785b2e1c4c5a02b6b1f9d649c16afae35fbba2
Grant Avery <grantavery@icloud.com>
2020-10-27 15:37:44 -0400

    Take two for CORS

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add CORS headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://grantavery.com");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
